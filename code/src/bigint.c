e0c37ffc6dff0f775d0e2849dffd9c0614d9b6d3
Mattia Gollub <mattia.gollub@hotmail.ch>
2016-03-25 17:55:45 +0100

    BigInt implementation progress

--- code/src/bigint.c
+++ code/src/bigint.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdlib.h>
 
 #include "bigint.h"
 #include "logging/logging.h"
@@ -13,7 +14,12 @@ void bigint_free(BigInt* a)
     a->significant_octets = 0;
 }
 
-bigint_from_uint64(BigInt* dest, uint64_t num)
+void bigint_from_uint32(BigInt* dest, uint32_t num)
+{
+    bigint_from_uint64(dest, (uint64_t)num);
+}
+
+void bigint_from_uint64(BigInt* dest, uint64_t num)
 {
     // Find number of octets needed
     uint64_t num_octets;
@@ -22,10 +28,49 @@ bigint_from_uint64(BigInt* dest, uint64_t num)
         uint64_t octet = num & (0xFF << (8 * (num_octets - 1)));
         if (octet != 0) break;
     }
-    if (i == 0) i = 1;
     
     // Create result object
-    BigInt result = { 0, 0, 0 };
+    dest->allocated_octets = num_octets;
+    dest->significant_octets = num_octets;
+    dest->octets = (char*)malloc(sizeof(char) * num_octets);
+    assert(dest->octets != 0);
+    
+    // Copy the number to the allocate doctets
+    for (uint64_t i = 0; i < num_octets; i++)
+    {
+        dest->octets[i] = (char)((num >> i * 8) & 0xFF);
+    }
+}
+
+void bigint_from_hex_string(BigInt* dest, const char* num)
+{
+    assert(num != 0);
+    
+    // Ignore possible leading zeros
+    uint64_t i = 0;
+    uint64_t prefix_digits = 0;
+    while (num[i] != '\0' && num[i] == '0')
+    {
+        prefix_digits++;
+        i++;
+    }
+    
+    // Find the number of octets needed
+    uint64_t digits = 0;
+    while (num[i] != '\0')
+    {
+        digits++;
+        i++;
+    }
+    uint64_t num_octets = (digits + 1) / 2;
+    
+    // Create result object
+    dest->allocated_octets = num_octets;
+    dest->significant_octets = num_octets;
+    dest->octets = (char*)malloc(sizeof(char) * num_octets);
+    assert(dest->octets != 0);
+    
+    // Copy digits
 }
 
 int bigint_are_equal(const BigInt* a, const BigInt* b)
@@ -47,4 +92,3 @@ int bigint_are_equal(const BigInt* a, const BigInt* b)
     
     return true;
 }
-
