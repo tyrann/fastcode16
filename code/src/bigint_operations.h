156eaca7dbd3ec2bd9ffe5eee8b797b2c566b03b
tyrann <thomas.j.rubeli@gmail.com>
2016-03-29 14:53:45 +0200

    Add operation interface for BigInt

---
+++ code/src/bigint_operations.h
@@ -0,0 +1,17 @@
+#include "bigint.h"
+
+void bigint_is_even(BigInt* a);
+
+void bigint_left_shift_inplace(BigInt* a);
+
+void bigint_right_shift_inplace(BigInt* a);
+
+void bigint_add_inplace(BigInt* a, BigInt* b);
+
+void bigint_mul_inplace(BigInt* a, BigInt* b);
+
+int bigint_is_greater(BigInt* a, BigInt* b);
+
+int bigint_is_equal(BigInt* a,BigInt* b);
+
+
