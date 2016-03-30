062be5240a65934b7f3acb938e4295a639b3be45
Christoph Heiniger <heinigec@student.ethz.ch>
2016-03-30 13:26:54 +0200

    unit test for copying points

--- code/test/ec_point_unittest.cc
+++ code/test/ec_point_unittest.cc
@@ -22,4 +22,24 @@ TEST(create_point, create_point)
     point_free(&p);
 }
 
+TEST(copy_point, copy_point)
+{
+    Point p, q;
+    BigInt x;
+    BigInt y;
+    bigint_from_hex_string(&x, "FFCC");
+    bigint_from_hex_string(&y, "CCFF");
+    create_point(&p, &x, &y);
+    point_copy(&q, &p);
+
+    ASSERT_TRUE(bigint_are_equal(&(p.x), &(q.x)));
+    ASSERT_TRUE(bigint_are_equal(&(p.y), &(q.y)));
+    ASSERT_FALSE(p.is_at_infinity);
+    ASSERT_FALSE(q.is_at_infinity);
+
+    bigint_free(&x);
+    bigint_free(&y);
+    point_free(&p);
+    point_free(&q);
+}
 
