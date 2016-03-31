1727e6910e5da4a39d9b7c8e6e7a858fc374594c
Christoph Heiniger <heinigec@student.ethz.ch>
2016-03-31 15:13:52 +0200

    reading the config file

---
+++ code/src/config/config.c
@@ -0,0 +1,126 @@
+#include "config.h"
+
+const char * KEYS[] = {
+    "p",
+    "a",
+    "b",
+    "G_x",
+    "G_y",
+    "n",
+    "h"
+};
+
+int read_config_file(EllipticCurveParameter *parameters, const char* filename)
+{
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if(fp == NULL)
+    {
+	return CONFIG_COULD_NOT_OPEN_FILE;
+    }
+
+    char buf[BUFFER_SIZE];
+    
+    int i = 0;
+    BigInt p, a, b, g_x, g_y, n, h;
+    Point g;
+    while (fgets (buf, sizeof(buf), fp) != NULL)
+    {
+	int length = strlen(buf);
+	// remove \n
+	buf[length-1] = '\0';
+	char *key;
+	char *val;
+	
+	if(parse_line(buf, &key, &val) && is_valid_key(key, i))
+	{
+	    if(!is_valid_hex_string(val))
+	    {
+		fclose(fp);
+		return CONFIG_INVALID_FORMAT;
+	    }
+	    switch(i)
+	    {
+	    case 0: bigint_from_hex_string(&p, val); break;
+	    case 1: bigint_from_hex_string(&a, val); break;
+	    case 2: bigint_from_hex_string(&b, val); break;
+	    case 3: bigint_from_hex_string(&g_x, val); break;
+	    case 4: bigint_from_hex_string(&g_y, val); break;
+	    case 5: bigint_from_hex_string(&n, val); break;
+	    case 6: bigint_from_hex_string(&h, val); break;
+	    }
+	}
+	else
+	{
+	    fclose(fp);
+	    return CONFIG_INVALID_FORMAT;
+	}
+	++i;
+	free(key);
+	free(val);
+    }
+
+    create_point(&g, &g_x, &g_y);
+    ec_create_parameters(parameters, &p, &a, &b, &g, &n, &h);
+    
+    bigint_free(&p);
+    bigint_free(&a);
+    bigint_free(&b);
+    point_free(&g);
+    bigint_free(&g_x);
+    bigint_free(&g_y);
+    bigint_free(&n);
+    bigint_free(&h);
+    fclose(fp);
+    return CONFIG_SUCCESS;
+}
+
+int parse_line(char *buf, char **key, char **val)
+{
+    const char t[] = "=";
+    char *token;
+    token = strtok(buf, t);
+    if(token == NULL)
+    {
+	return 0;
+    }
+    int n = strlen(token);
+    *key = malloc(sizeof(char)*(n+1));
+    strncpy(*key, token, n+1);
+    token = strtok(NULL, t);
+    if(token == NULL)
+    {
+	free(*key);
+	return 0;
+    }
+    n = strlen(token);
+    *val = malloc(sizeof(char)*(n+1));
+    strncpy(*val, token, n+1);
+
+    return 1;
+}
+
+int is_valid_key(char *key, int i)
+{
+    if(strcmp(KEYS[i], key) == 0)
+    {
+	return 1;
+    }
+    return 0;
+}
+
+int is_valid_hex_string(const char *val) 
+{
+    if(strlen(val) == 0)
+    {
+	return 0;
+    }
+    for(unsigned int i = 0; i < strlen(val); i++)
+    {
+	if(!isxdigit(val[i]))
+	{
+	    return 0;
+	}
+    }
+    return 1;
+}
---
+++ code/test/config/config_unittest.cc
@@ -0,0 +1,54 @@
+extern "C" {
+    #include "../../src/config/config.h"
+    #include "../../src/ec_parameters.h"
+}
+#include "gtest/gtest.h"
+#include <unistd.h>
+#include <stdio.h>
+
+TEST(read_config_file, file_does_not_exists)
+{
+    const char file_name[] = "config_file_does_not_exists.txt";
+    EllipticCurveParameter params;
+    int code = read_config_file(&params, file_name);
+    ASSERT_EQ(CONFIG_COULD_NOT_OPEN_FILE, code);
+}
+
+TEST(parse_line, no_equal_sign)
+{
+    char buffer[] = "testtest";
+    char *key;
+    char *val;
+    ASSERT_FALSE(parse_line(buffer, &key, &val));
+}
+
+TEST(parse_line, correct_example)
+{
+    char buffer[] = "p=1234";
+    char *key;
+    char *val;
+    ASSERT_TRUE(parse_line(buffer, &key, &val));
+    ASSERT_TRUE(strcmp("p", key) == 0);
+    ASSERT_TRUE(strcmp("1234", val) == 0);
+    free(key);
+    free(val);
+}
+
+TEST(is_valid_hex_string, empty_string) 
+{
+    ASSERT_FALSE(is_valid_hex_string(""));
+}
+
+TEST(is_valid_hex_string, invalid_inputs) 
+{
+    ASSERT_FALSE(is_valid_hex_string("11111_"));
+    ASSERT_FALSE(is_valid_hex_string("G"));
+    ASSERT_FALSE(is_valid_hex_string("?"));
+}
+
+TEST(is_valid_hex_string, correct_inputs) 
+{
+    ASSERT_TRUE(is_valid_hex_string("11111"));
+    ASSERT_TRUE(is_valid_hex_string("ABCD"));
+    ASSERT_TRUE(is_valid_hex_string("1111BBBCCC"));
+}
