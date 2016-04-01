07a326edc7453d809f3fee432acb070c23b34981
Christoph Heiniger <heinigec@student.ethz.ch>
2016-04-01 10:44:45 +0200

    refactoring points

---
+++ code/src/ec_point_operations.h
@@ -0,0 +1,16 @@
+
+#ifndef __EC_POINT_OPERATIONS_H_
+#define __EC_POINT_OPERATIONS_H_
+
+#include <stdio.h>
+#include "ec_point_structure.h"
+#include "ec_parameters.h"
+
+/* Addition of two points on a curve*/
+void ec_point_add(Point *result, const Point *a, const Point *b, EllipticCurveParameter *p);
+
+/* Multiplication of a point on a curve by a scalar*/
+/* result = d*P */
+void ec_point_mul(Point *result, const BigInt *d, const Point *P, EllipticCurveParameter *p);
+
+#endif 
