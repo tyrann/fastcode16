3064ff9c09ad244ce63771517dc5c0f0be01de36
Christoph Heiniger <heinigec@student.ethz.ch>
2016-04-01 10:47:09 +0200

    const

--- code/src/ec_point_operations.h
+++ code/src/ec_point_operations.h
@@ -7,10 +7,10 @@
 #include "ec_parameters.h"
 
 /* Addition of two points on a curve*/
-void ec_point_add(Point *result, const Point *a, const Point *b, EllipticCurveParameter *p);
+void ec_point_add(Point *result, const Point *a, const Point *b, const EllipticCurveParameter *p);
 
 /* Multiplication of a point on a curve by a scalar*/
 /* result = d*P */
-void ec_point_mul(Point *result, const BigInt *d, const Point *P, EllipticCurveParameter *p);
+void ec_point_mul(Point *result, const BigInt *d, const Point *P, const EllipticCurveParameter *p);
 
 #endif 
