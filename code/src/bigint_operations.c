ce2c398bdd14d4ce46ebc2bfa68a20973aa77f19
tyrann <thomas.j.rubeli@gmail.com>
2016-04-03 16:02:26 +0200

    Fix indentation

--- code/src/bigint_operations.c
+++ code/src/bigint_operations.c
@@ -287,88 +287,88 @@ void bigint_sub_inplace(BigInt* a, BigInt* b)
 	} 
 	else 
 	{
-	BIGINT_ASSERT_VALID(a);
-    BIGINT_ASSERT_VALID(b);
+		BIGINT_ASSERT_VALID(a);
+		BIGINT_ASSERT_VALID(b);
 
-	// count stores the minimum number of digits between a and b
-	uint64_t count = (a->significant_octets > b->significant_octets) ? b->significant_octets : a->significant_octets;	
-	uint64_t i = 0;
-	uint8_t back_carry;
-	uint8_t next_back_carry;
-   	// next_back_carry is created to handle cases where back_carry is needed for i,  yet the both i+1 and i+2 bytes are zeros
-	// Execute the sub from the lowest digits to the digits of count
-	for (; i < count; i++)
-    {
-		uint32_t atemp = (uint32_t)a->octets[i];
-		uint32_t btemp = (uint32_t)b->octets[i];
-		back_carry = 0;
-		back_carry = (atemp < btemp);
+		// count stores the minimum number of digits between a and b
+		uint64_t count = (a->significant_octets > b->significant_octets) ? b->significant_octets : a->significant_octets;	
+		uint64_t i = 0;
+		uint8_t back_carry = 0;
+		uint8_t next_back_carry = 0;
+		// next_back_carry is created to handle cases where back_carry is needed for i,  yet the both i+1 and i+2 bytes are zeros
+		// Execute the sub from the lowest digits to the digits of count
+		for (; i < count; i++)
+		{
+			uint32_t atemp = (uint32_t)a->octets[i];
+			uint32_t btemp = (uint32_t)b->octets[i];
+			back_carry = 0;
+			back_carry = (atemp < btemp);
 
-		// Two-complement is stored in btemp
-		btemp = ~((uint32_t)b->octets[i])+1;
+			// Two-complement is stored in btemp
+			btemp = ~((uint32_t)b->octets[i])+1;
+			
+			if(back_carry || next_back_carry)
+			{
+				
+				// If possible get a unit from the byte above
+				if ((uint32_t)a->octets[i+1]!=0) 
+				{
+					a->octets[i+1] -= 1;
+				}	
+				// Else we need to look further up
+				else
+				{
+					a->octets[i+1] = 0xFF;
+					next_back_carry = 1;
+				}				
+			}
+			atemp = atemp + btemp;
+			a->octets[i] = atemp;	
+		}
 		
+		// We might need to keep retrieving a unit until we reach a non-zero byte 
+		int stopFlag = 0;
+		i=i+1;
 		if(back_carry || next_back_carry)
 		{
-			
-			// If possible get a unit from the byte above
-			if ((uint32_t)a->octets[i+1]!=0) 
+			for (; (i < a->significant_octets)&&(!stopFlag); i++)
 			{
-				a->octets[i+1] -= 1;
-			}	
-			// Else we need to look further up
+				if ((uint32_t)a->octets[i] == 0)
+				{
+					a->octets[i] = 0xFF;
+				}
+				else 
+				{
+					a->octets[i] -= 1;
+					stopFlag = 1; 
+				}
+			}
+		}
+		// Empty most significant bytes that are zeros 
+		uint64_t emptyBytes = 0;
+		stopFlag = 0;
+		for (uint64_t i = a->significant_octets-1; (i>0) && (!stopFlag); i--)
+		{
+			if(a->octets[i]==0)
+			{
+				emptyBytes +=1;
+			}
 			else
 			{
-				a->octets[i+1] = 0xFF;
-				next_back_carry = 1;
-			}				
+				stopFlag = 1;
+			}
 		}
-		atemp = atemp + btemp;
-		a->octets[i] = atemp;	
-    }
-	
-	// We might need to keep retrieving a unit until we reach a non-zero byte 
-	int stopFlag = 0;
-	i=i+1;
-	if(back_carry || next_back_carry)
-	{
-	for (; (i < a->significant_octets)&&(!stopFlag); i++)
-    {
-		if ((uint32_t)a->octets[i] == 0)
+		// Deallocate most significant bytes that are zero 
+		if(emptyBytes > 0)
 		{
-			a->octets[i] = 0xFF;
-		}
-		else 
-		{
-			a->octets[i] -= 1;
-			stopFlag = 1; 
-		}
-    }
-    }
-	// Empty higher significant bytes that are zeros 
-	uint64_t emptyBytes = 0;
-	stopFlag = 0;
-	for (uint64_t i = a->significant_octets-1; (i>0)&&(!stopFlag); i--)
-	{
-		if((uint32_t)a->octets[i]==0)
-		{
-			emptyBytes +=1;
-		}
-		else
-		{
-			stopFlag = 1;
-		}
-	}
-	// Deallocate most significant bytes that are zero 
-	if(emptyBytes > 0)
-	{
-		a->significant_octets -= emptyBytes;
-		a->allocated_octets -= emptyBytes;
-		uchar* newOctets = realloc(a->octets, sizeof(uchar) * a->significant_octets);
-		if (newOctets) 
-		{
-			a->octets = newOctets;
+			a->significant_octets -= emptyBytes;
+			a->allocated_octets -= emptyBytes;
+			uchar* new_octets = realloc(a->octets, sizeof(uchar) * a->significant_octets);
+			if (new_octets) 
+			{
+				a->octets = new_octets;
+			}	
 		}	
-	}	
 	}
 }
 
