c797cccdb1967b9dfc33295a23d1ba2c881d75ec
tyrann <thomas.j.rubeli@gmail.com>
2016-04-26 11:08:52 +0200

    Add automatic opcount header file

---
+++ code/src/opcount/opcount.h
@@ -0,0 +1,12 @@
+#ifndef __OPCOUNT_H_
+#define __OPCOUNT_H_
+
+// Trigger operation count 
+#define OP_COUNT_NONE 0      // Counts nothing
+#define OP_COUNT_ALL 1       // Counts all
+
+#ifndef OP_COUNT
+#define OP_COUNT OP_COUNT_ALL
+#endif
+
+#endif
