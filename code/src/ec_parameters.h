424a310f431c38b9abbd5b71acee92f7a9ec51f7
Christoph Heiniger <heinigec@student.ethz.ch>
2016-05-05 15:04:31 +0200

    fixed ec_point_operations_unittest

--- code/src/ec_parameters.h
+++ code/src/ec_parameters.h
@@ -15,27 +15,13 @@ typedef struct
 	BigInt h;
 } EllipticCurveParameter;
 
+typedef enum {SECP192K1, SECP521R1} CurveParameter;
 
-/*Elliptic Curve Domain Parameters over Fp Validation Primitive
- *
- * Input:
- * Elliptic curve domain parameters over Fp
- *Output:
- * An indication of whether the elliptic curve domain parameters are valid or not 
- * 
- * The return value should be 0 if invalid and 1 otherwise*/
-int ec_validation(EllipticCurveParameter e, int security_level); 
+/*
+This function generates parameters, that are often used in crypto
 
-
-/*Elliptic Curve Domain Parameters over Fp Generation Primitive
- *
- * Input:
- * The approximate security level in bits required from the elliptic curve domain parameters
- * this must be an integer t  in {  80  ,  112  ,  128  ,  192  ,  256  }  . Optionally, a seed value S
- *Output:
- *  Elliptic curve domain parameters over Fp
- * */
-EllipticCurveParameter ec_generation(int security_level);
+*/
+void ec_generate_parameter(EllipticCurveParameter *parameter, const CurveParameter name);
 
 void ec_free(EllipticCurveParameter *ec);
 
