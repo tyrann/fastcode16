7f68e607d1837f6e8165c9cd3f3ae2b7053b1caf
Mattia Gollub <mattia.gollub@hotmail.ch>
2016-05-15 17:21:46 +0000

    ec_add_inplace added, bigint copies reduced

--- code/src/ec_point_operations.c
+++ code/src/ec_point_operations.c
@@ -153,15 +153,165 @@ void ec_point_add(Point *result, const Point *a, const Point *b, const EllipticC
     }
 }
 
+void ec_point_add_inplace(Point *a, const Point *b, const EllipticCurveParameter *params)
+{	
+    assert(point_is_on_curve(a, params));
+    assert(point_is_on_curve(b, params));
+	
+    // SEC 1: Elliptic Curve Cryptography Certicom Research Version 2.0 page 7
+    
+    // Rule 1
+    if(a->is_at_infinity && b->is_at_infinity)
+    {
+		a->is_at_infinity = 1;
+    }
+    // Rule 2
+    else if(a->is_at_infinity)
+    {
+		point_copy(a, b);
+    }
+    else if(b->is_at_infinity)
+    {
+		// Nothing to do here
+    }
+    // Rule 5
+    else if(bigint_are_equal(a->x, b->x) && bigint_are_equal(a->y, b->y)
+		&& !bigint_are_equal(a->y, bigint_zero))
+    {
+		// Get BigInt objects
+		BigInt numerator = GET_BIGINT_PTR(BI_POINTADD_NUMERATOR_TAG);
+		BigInt numerator_rev = GET_BIGINT_PTR(BI_POINTADD_NUMERATORREV_TAG);
+		BigInt denominator = GET_BIGINT_PTR(BI_POINTADD_DENOMINATOR_TAG);
+		BigInt lambda = GET_BIGINT_PTR(BI_POINTADD_LAMBDA_TAG);
+		BigInt lambda_squared = GET_BIGINT_PTR(BI_POINTADD_LAMBDASQUARED_TAG);
+		BigInt x_twice = GET_BIGINT_PTR(BI_POINTADD_XTWICE_TAG);
+		BigInt y_1 = GET_BIGINT_PTR(BI_POINTADD_Y1_TAG);
+		BigInt y_3 = GET_BIGINT_PTR(BI_POINTADD_Y3_TAG);
+		BigInt x_1_minus_x_3 = GET_BIGINT_PTR(BI_POINTADD_X1MINUSX3_TAG);
+
+		// numerator
+		montgomery_mul(numerator, a->x, a->x, params->p);
+		__montgomery_revert(numerator_rev, numerator, params->p);
+
+		bigint_copy(numerator, numerator_rev);
+		bigint_left_shift_inplace(numerator);
+		bigint_add_inplace(numerator, numerator_rev);
+		bigint_add_inplace(numerator, params->a);
+		bigint_modulo_inplace(numerator, params->p);
+	
+		// denominator
+		bigint_copy(denominator, a->y);
+		bigint_copy(y_1, a->y);
+		bigint_left_shift_inplace(denominator);
+		bigint_modulo_inplace(denominator, params->p);
+
+		// calculate lambda
+		bigint_divide(lambda, numerator, denominator, params->p);
+
+		//calculate x
+		bigint_copy(x_1_minus_x_3, a->x);
+		bigint_copy(x_twice, a->x);
+		
+		montgomery_mul(lambda_squared, lambda, lambda, params->p);
+		__montgomery_revert(a->x, lambda_squared, params->p);
+
+		bigint_left_shift_inplace(x_twice);
+		bigint_modulo_inplace(x_twice, params->p);
+		
+		bigint_add_inplace(a->x, params->p);
+		bigint_sub_inplace(a->x, x_twice);
+		bigint_modulo_inplace(a->x, params->p);
+	
+		// calculate y
+		
+		// x_1 - x_3
+		bigint_add_inplace(x_1_minus_x_3, params->p);
+		bigint_sub_inplace(x_1_minus_x_3, a->x);
+
+		// calculate lambda(x_1 - x_3)
+		montgomery_mul(y_3, lambda, x_1_minus_x_3, params->p);
+		__montgomery_revert(a->y, y_3, params->p);
+
+		bigint_add_inplace(a->y, params->p);
+		bigint_sub_inplace(a->y, y_1);
+		bigint_modulo_inplace(a->y, params->p);
+
+		a->is_at_infinity = 0;
+    } 
+    // Rule 3
+    else if(bigint_are_equal(a->x, b->x))
+    {
+		a->is_at_infinity = 1;
+    }
+    // Rule 4
+    else if(!bigint_are_equal(a->x, b->x))
+    {	
+		// Get BigInt objects
+		BigInt numerator = GET_BIGINT_PTR(BI_POINTADD_NUMERATOR_TAG);
+		BigInt denominator = GET_BIGINT_PTR(BI_POINTADD_DENOMINATOR_TAG);
+		BigInt lambda = GET_BIGINT_PTR(BI_POINTADD_LAMBDA_TAG);
+		BigInt lambda_squared = GET_BIGINT_PTR(BI_POINTADD_LAMBDASQUARED_TAG);
+		BigInt x_1_add_inverse = GET_BIGINT_PTR(BI_POINTADD_X1ADDINVERSE_TAG);
+		BigInt y_1_add_inverse = GET_BIGINT_PTR(BI_POINTADD_Y1ADDINVERSE_TAG);
+		BigInt y_3 = GET_BIGINT_PTR(BI_POINTADD_Y3_TAG);
+		BigInt x_1_minus_x_3 = GET_BIGINT_PTR(BI_POINTADD_X1MINUSX3_TAG);
+	
+		// Compute inverses
+		bigint_copy(x_1_add_inverse, params->p);
+		bigint_sub_inplace(x_1_add_inverse, a->x);
+		
+		bigint_copy(y_1_add_inverse, params->p);
+		bigint_sub_inplace(y_1_add_inverse, a->y);
+	
+		//Calculate y_2 - y_1
+		bigint_copy(numerator, b->y);
+		bigint_add_inplace(numerator, y_1_add_inverse);
+		bigint_modulo_inplace(numerator, params->p);
+	
+		//Calculate x_2 - x_1		
+		bigint_copy(denominator, b->x);
+		bigint_add_inplace(denominator, x_1_add_inverse);
+		bigint_modulo_inplace(denominator, params->p);
+	
+		//Calculate lambda
+		bigint_divide(lambda, numerator, denominator, params->p);
+
+		//Calculate lambda^2
+		bigint_copy(x_1_minus_x_3, a->x);
+		montgomery_mul(lambda_squared, lambda, lambda, params->p);
+		__montgomery_revert(a->x, lambda_squared, params->p);
+	
+		bigint_add_inplace(a->x, x_1_add_inverse);
+		bigint_add_inplace(a->x, params->p);
+		bigint_sub_inplace(a->x, b->x);
+		bigint_modulo_inplace(a->x, params->p);
+
+		// Calculate y_3
+		bigint_add_inplace(x_1_minus_x_3, params->p);
+		bigint_sub_inplace(x_1_minus_x_3, a->x);
+
+		montgomery_mul(y_3, lambda, x_1_minus_x_3, params->p);
+		__montgomery_revert(a->y, y_3, params->p);
+		
+		bigint_add_inplace(a->y, y_1_add_inverse);
+		bigint_modulo_inplace(a->y, params->p);
+	
+		a->is_at_infinity = 0;
+    }
+    else
+    {
+		assert("ec_point_add - this code should not be reached");
+    }
+}
+
 void ec_point_mul(Point *result, const BigInt d, const Point *P, const EllipticCurveParameter *params)
 {
-	Point p1, p2, q2;
-	
-	create_point_from_uint32(&p1, BI_POINTMUL_P1X_TAG, BI_POINTMUL_P1Y_TAG, 0, 0);
+	Point p2;
+
 	create_point_from_uint32(&p2, BI_POINTMUL_P2X_TAG, BI_POINTMUL_P2Y_TAG, 0, 0);
-	create_point_from_uint32(&q2, BI_POINTMUL_Q2X_TAG, BI_POINTMUL_Q2Y_TAG, 0, 0);
+	bigint_copy(result->x, bigint_zero);
+	bigint_copy(result->y, bigint_zero);
 	result->is_at_infinity = 1;
-	q2.is_at_infinity = 1;
 	
     point_copy(&p2, P);
 
@@ -172,11 +322,9 @@ void ec_point_mul(Point *result, const BigInt d, const Point *P, const EllipticC
 			__COUNT_OP(&global_opcount,2);
 	    	if(d->octets[i] & (1 << j)) 
 	    	{
-				ec_point_add(result, &q2, &p2, params);
-				point_copy(&q2, result);	
+				ec_point_add_inplace(result, &p2, params);	
 	   		}
-	    	ec_point_add(&p1, &p2, &p2, params);
-	    	point_copy(&p2, &p1);
+	    	ec_point_add_inplace(&p2, &p2, params);
 			__COUNT_OP(&global_index_count,1);
 		}
 
--- code/test/ec_point_operations_unittest.cc
+++ code/test/ec_point_operations_unittest.cc
@@ -202,6 +202,162 @@ TEST(ec_point_add, large_number)
     bigint_destroy_buffer();
 }
 
+TEST(ec_point_add_inplace, inf_plus_inf)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    create_parameters_1(&params);
+    Point a, b, expected;
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 0, 0);
+    create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 0, 0);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 0, 0);
+    a.is_at_infinity = 1;
+    b.is_at_infinity = 1;
+    expected.is_at_infinity = 1;
+
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
+TEST(ec_point_add_inplace, a_plus_inf)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    create_parameters_1(&params);
+    Point a, b, expected;
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 1, 8);
+    create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 0, 0);
+    b.is_at_infinity = 1;
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 1, 8);
+
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
+TEST(ec_point_add_inplace, inf_plus_b)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    create_parameters_1(&params);
+    Point a, b, expected;
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 0, 0);
+    a.is_at_infinity = 1;
+    create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 1, 8);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 1, 8);
+
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
+TEST(ec_point_add_inplace, a_plus_a_curve1)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    create_parameters_2(&params);
+    Point a, b, expected;
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 188, 93);
+    create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 188, 93);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 2505, 3126);
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 692, 150);
+	create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 692, 150);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 6111, 95);
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 6026, 210);
+	create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 6026, 210);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 3075, 6164);
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
+TEST(ec_point_add_inplace, a_plus_b)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    create_parameters_2(&params);
+    Point a, b, expected;
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 2, 2903);
+	create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 2, 5024);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 0, 0);
+	expected.is_at_infinity = 1;
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 0, 0);
+	create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 0, 0);
+	create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 0, 0);
+	expected.is_at_infinity = 1;
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
+TEST(ec_point_add_inplace, a_plus_b_rule4)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    create_parameters_2(&params);
+    Point a, b, expected;
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 6026, 210);
+	create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 2, 2903);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 4555, 1855);
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    create_point_from_uint32(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, 4, 5017);
+	create_point_from_uint32(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, 475, 2499);
+    create_point_from_uint32(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, 6563, 6553);
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
+TEST(ec_point_add_inplace, large_number)
+{
+    bigint_create_buffer();
+    
+    EllipticCurveParameter params;
+    ec_generate_parameter(&params, SECP192K1);
+    Point a, b, expected;
+
+    create_point_from_hex(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, "DB4FF10EC057E9AE26B07D0280B7F4341DA5D1B1EAE06C7D", "9B2F2F6D9C5628A7844163D015BE86344082AA88D95E2F9D");
+	create_point_from_hex(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, "DB4FF10EC057E9AE26B07D0280B7F4341DA5D1B1EAE06C7D", "9B2F2F6D9C5628A7844163D015BE86344082AA88D95E2F9D");
+    create_point_from_hex(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, "F091CF6331B1747684F5D2549CD1D4B3A8BED93B94F93CB6", "FD7AF42E1E7565A02E6268661C5E42E603DA2D98A18F2ED5");
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    create_point_from_hex(&a, BI_TESTS_AX_TAG, BI_TESTS_AY_TAG, "DB4FF10EC057E9AE26B07D0280B7F4341DA5D1B1EAE06C7D", "9B2F2F6D9C5628A7844163D015BE86344082AA88D95E2F9D");
+	create_point_from_hex(&b, BI_TESTS_BX_TAG, BI_TESTS_BY_TAG, "F091CF6331B1747684F5D2549CD1D4B3A8BED93B94F93CB6", "FD7AF42E1E7565A02E6268661C5E42E603DA2D98A18F2ED5");
+    create_point_from_hex(&expected, BI_TESTS_EXPECTEDX_TAG, BI_TESTS_EXPECTEDY_TAG, "6E43B7DCAE2FD5E0BF2A1BA7615CA3B9065487C9A67B4583", "C48DCEA47AE08E84D5FEDC3D09E4C19606A290F7A19A6A58");
+    ec_point_add_inplace(&a, &b, &params);
+    ASSERT_TRUE(point_are_equal(&a, &expected));
+
+    bigint_destroy_buffer();
+}
+
 TEST(ec_point_mul, zero_times_point)
 {
     bigint_create_buffer();
