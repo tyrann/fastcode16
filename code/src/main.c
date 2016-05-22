3baa8e839ccf2c4e0c7bbd391a271c73e2e0ca6d
Christoph Heiniger <heinigec@student.ethz.ch>
2016-05-22 14:41:46 +0200

    removed "dangerous" dummy tag

--- code/src/main.c
+++ code/src/main.c
@@ -88,8 +88,8 @@ void compute_ECDH(char* dURand, char* dVRand, int keyLength)
     double r;  
     r = cycles / num_runs;
     printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n", r, r/(FREQUENCY), (FREQUENCY)/1e6); 
-    BigInt sharedInfoU_conv = GET_BIGINT_PTR(BI_DUMMY1_TAG);
-    BigInt sharedInfoV_conv = GET_BIGINT_PTR(BI_DUMMY2_TAG);
+    BigInt sharedInfoU_conv = GET_BIGINT_PTR(BI_MAIN_SHAREDINFOU_CONV_TAG);
+    BigInt sharedInfoV_conv = GET_BIGINT_PTR(BI_MAIN_SHAREDINFOV_CONV_TAG);
     __montgomery_revert(sharedInfoU_conv, sharedInfoU, params.p);
     __montgomery_revert(sharedInfoV_conv, sharedInfoV, params.p);
     char *infoU = bigint_to_hex_string(sharedInfoU_conv);
