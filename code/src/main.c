462b6a024e3683fdfa7f789eba8df20e96916687
zhthantao <zhthantao@hotmail.com>
2016-05-24 11:36:48 +0200

    Add benchmark for each functions

--- code/src/main.c
+++ code/src/main.c
@@ -6,7 +6,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#define NUM_RUNS 2
 #define CYCLES_REQUIRED 1e8
 #define FREQUENCY 1.8e9
 #define CALIBRATE
@@ -125,10 +124,152 @@ double rdtsc(char* dURand, char* dVRand, int keyLength) {
   return cycles;
 }
 
+
+unsigned short int n;
+extern uint64_t global_opcount; 
+extern uint64_t global_index_count; 
+
+/*
+ * Test left shift 
+*/
+void test_Left_Shift()
+{
+    myInt64 cycles;
+    myInt64 start;
+    int num_runs = NUM_RUNS;
+
+    start = start_tsc();
+	BigInt a;	
+	bigint_from_hex_string(&a, "8cd2c5edd23d55f01c9007ffffffc006");
+
+	for(int i = 0; i < num_runs; i++)
+    {
+    bigint_left_shift_inplace(&a);      	
+    }
+    cycles = stop_tsc(start);
+
+    global_opcount = global_opcount/num_runs;
+    global_index_count = global_index_count/num_runs;
+    double r;  
+    r = cycles / num_runs;
+    printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n", r, r/(FREQUENCY), (FREQUENCY)/1e6); 
+	bigint_free(&a);
+}
+
+
+/*
+ * Test Addition
+*/
+void test_Add()
+{
+    myInt64 cycles;
+    myInt64 start;
+    int num_runs = NUM_RUNS;
+
+	BigInt a;
+	bigint_from_hex_string(&a, "8cd2c5edd23d55f01c9007ffffffc006");
+	BigInt b;
+	bigint_from_hex_string(&b, "9007ffffffc0068cd2c5edd23d55f01c");
+
+    start = start_tsc();
+	for(int i = 0; i < num_runs; i++)
+    {
+    bigint_add_inplace(&a, &b);
+    }
+    cycles = stop_tsc(start);
+
+    global_opcount = global_opcount/num_runs;
+    global_index_count = global_index_count/num_runs;
+    double r;  
+    r = cycles / num_runs;
+    printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n", r, r/(FREQUENCY), (FREQUENCY)/1e6);   
+	bigint_free(&a);
+    bigint_free(&b);
+}
+
+
+
+/*
+ * Test montgomery multiplication
+*/
+void test_Mult()
+{
+    myInt64 cycles;
+    myInt64 start;
+    int num_runs = NUM_RUNS;
+	
+	BigInt a;
+	bigint_from_hex_string(&a, "8cd2c5edd23d55f01c9007ffffffc006");
+	BigInt b;
+	bigint_from_hex_string(&b, "9007ffffffc0068cd2c5edd23d55f01c");
+	BigInt p;
+	bigint_from_hex_string(&p, "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFEE37");
+	
+	BigInt res;
+
+    start = start_tsc();
+	for(int i = 0; i < num_runs; i++)
+    {
+    montgomery_mul(&res, &a, &b, &p);
+    }
+    cycles = stop_tsc(start);
+
+    global_opcount = global_opcount/num_runs;
+    global_index_count = global_index_count/num_runs;
+    double r;  
+    r = cycles / num_runs;
+    printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n", r, r/(FREQUENCY), (FREQUENCY)/1e6); 
+	bigint_free(&a);
+    bigint_free(&b);
+	bigint_free(&p);
+    bigint_free(&res);	
+}
+
+
+
+/*
+ * Test division
+*/
+
+void test_Div()
+{
+    myInt64 cycles;
+    myInt64 start;
+    int num_runs = NUM_RUNS;
+	
+	BigInt a;
+	bigint_from_hex_string(&a, "7cd73b6fc007dfee34a23caf363ae67e8bb8782600000000032accceb");
+	BigInt b;
+	bigint_from_hex_string(&b, "c69d8b898f0e43b4643a018e7b0569de6f8cf328e0bf6d59ace4e3bc2ca28d10");
+	BigInt p;
+	bigint_from_hex_string(&p, "ffffffff00000001000000000000000000000000ffffffffffffffffffffffff");
+	BigInt res;	
+	
+    start = start_tsc();
+	for(int i = 0; i < num_runs; i++)
+    {
+	bigint_divide(&res, &b, &a, &p);
+    }
+    cycles = stop_tsc(start);
+
+    global_opcount = global_opcount/num_runs;
+    global_index_count = global_index_count/num_runs;
+    double r;  
+    r = cycles / num_runs;
+    printf("RDTSC instruction:\n %lf cycles measured => %lf seconds, assuming frequency is %lf MHz. (change in source file if different)\n", r, r/(FREQUENCY), (FREQUENCY)/1e6);     
+	bigint_free(&a);
+    bigint_free(&b);
+	bigint_free(&p);
+    bigint_free(&res);	
+}
+
+
+
 int main(){
   global_opcount = 0;
   global_index_count = 0; 
 
+  /*
   // secp192
   rdtsc("FEFFFFFFFFFFFFFFFFFFFFFE62F2FC170F69466A74DEFD8D","FEFFFFFFFFFFFFFFFFFFFFFE26F2FC710F69466A74DEFD8D",192); 
   global_opcount = 0;
@@ -151,6 +292,34 @@ int main(){
   // secp521
   rdtsc("01EFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA15868783BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409","01EFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFA51868738BF2F966B7FCC0148F709A5D03BB5C9B8899C47AEBB6FB71E91386409",521);
   global_opcount = 0;
-  global_index_count = 0;  
+  global_index_count = 0; 
+
+*/
+
+	// Test left shift
+	printf("============Test ShiftLeft============\n");
+
+	printf("ECC ShiftLeft\n");
+	test_Left_Shift();
+
+	
+	// Test add 
+	printf("============Test Add============\n");
+	printf("ECC Addition\n");
+	test_Add();
+
+	
+	// Test montgomery multiplication 
+	printf("============Test Mult============\n");
+	printf("ECC mont-multiplication\n");
+	test_Mult();
+
+	
+	// Test division
+	printf("============Test Div============\n");
+	printf("ECC Division\n");
+	test_Div();
+	
+	
   return 0;
 }
